1159420541f7f7b9f4fe04874c47c613aecb82bd
prabhudatta22 <63843164+prabhudatta22@users.noreply.github.com>
2020-08-29 16:48:50 +0530

    Update ExpenseServiceTest.java

--- src/test/java/com/expense/service/ExpenseServiceTest.java
+++ src/test/java/com/expense/service/ExpenseServiceTest.java
@@ -63,6 +63,7 @@ public class ExpenseServiceTest {
 		when(expenseRepository.findAll()).thenReturn(expenseList);
 
 		List<Expense> expenseList1 = expenseService.getExpense();
+		System.out.println("updated code !!!")
 		assertEquals(1, expenseList1.size());
 	}
 	
